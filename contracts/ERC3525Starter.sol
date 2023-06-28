066522fbc88eaf05bd9bdedb6fc3986742357c5a
rolandm99 <manfulmweze99@gmail.com>
2023-06-28 18:59:00 +0200

    Add the SVG image for ERC3525 visualization

--- contracts/ERC3525Starter.sol
+++ contracts/ERC3525Starter.sol
@@ -17,4 +17,25 @@ constructor(address owner_)
      "ERC3525Starter: only owner can mint");
    _mint(to_, slot_, amount_);
     }
+
+     function tokenURI(uint256 tokenId_) public view virtual override returns (string memory) {
+        return string(
+            abi.encodePacked(
+                '<svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">',
+                ' <g> <title>Layer 1</title>',
+                '  <rect id="svg_1" height="600" width="600" y="0" x="0" stroke="#000" fill="#000000"/>',
+                '  <text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_2" y="340" x="200" stroke-width="0" stroke="#000" fill="#ffffff">TokenId: ',
+                tokenId_.toString(),
+                '</text>',
+                '  <text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_3" y="410" x="200" stroke-width="0" stroke="#000" fill="#ffffff">Balance: ',
+                balanceOf(tokenId_).toString(),
+                '</text>',
+                '  <text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_3" y="270" x="200" stroke-width="0" stroke="#000" fill="#ffffff">Slot: ',
+                slotOf(tokenId_).toString(),
+                '</text>',
+                '  <text xml:space="preserve" text-anchor="start" font-family="Noto Sans JP" font-size="24" id="svg_4" y="160" x="150" stroke-width="0" stroke="#000" fill="#ffffff">ERC3525 GETTING STARTED</text>',
+                ' </g> </svg>'
+            )
+        );
+     }
 }
