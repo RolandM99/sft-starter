b2565fc770fc9326451410ece50b877f9165ee71
rolandm99 <manfulmweze99@gmail.com>
2023-06-30 12:36:27 +0200

    Implement the script for deploying

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner] = await ethers.getSigners();
+  const ERC3525Starter = await ethers.getContractFactory("ERC3525Starter");
+  const erc3525Starter = await ERC3525Starter.deploy(owner.address);
 
-  const lockedAmount = ethers.parseEther("0.001");
+  await erc3525Starter.deployed();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("ERC3525Starter deployed to:", erc3525Starter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
